52ca2d68ec4c9979fd6ea3a35e26b573d8534889
Raian M. Gil del Rosario <rgil@lanzasoftware.com>
2023-12-04 12:33:13 -0400

    Secret config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import store from '../data/store'
 import { AppFrame } from '../layout/index'
 
 const MyApp = ({ Component, pageProps}:AppProps) => {
-  console.log('api url is', process.env.NEXT_PUBLIC_API_URL)
+  console.log('api url is', process.env.NEXT_PUBLIC_API_URL, process.env.API_URL)
   return (
     <Provider store={store}>
       <React.StrictMode>
