ebfbdf36be07a9135248f440915f0ce8e4132abf
Raian M. Gil del Rosario <79413096+RaianGil@users.noreply.github.com>
2024-02-14 21:08:41 -0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { MainContainer } from '@/feats/general'
 import React from 'react'
 
 const home = ({dispatch, router}) => {
+  console.log('Prueba', process.env.PRUEBA)
   return <>
     <MainContainer dispatch={dispatch} router={router}/>
   </>
